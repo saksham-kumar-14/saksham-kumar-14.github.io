e9ba5747b76a209a7ed81652dd2040c9b1855426
Saksham <sakshamkumar1415@gmail.com>
2025-07-04 23:00:49 +0530

    add about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import Icons from 'astro-icon';
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   integrations: [Icons()],
   site: 'https://saksham-kumar-14.github.io',
 });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
